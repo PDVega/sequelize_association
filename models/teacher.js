10109c46257758b520710fc05ef8c700e1cb72cf
pdvega <pdvegaa@gmail.com>
2017-07-14 00:22:50 +0700

    migration create addColumn subject_id on Teachers Table

--- models/teacher.js
+++ models/teacher.js
@@ -3,7 +3,8 @@ module.exports = function(sequelize, DataTypes) {
   var Teacher = sequelize.define('Teacher', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email: DataTypes.STRING
+    email: DataTypes.STRING,
+    subject_id: DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function(models) {
