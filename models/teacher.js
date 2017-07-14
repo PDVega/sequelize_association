082ad0400b748e70163d439146310e79a980f530
pdvega <pdvegaa@gmail.com>
2017-07-14 08:04:57 +0700

    teacher validation email done

--- models/teacher.js
+++ models/teacher.js
@@ -3,7 +3,23 @@ module.exports = function(sequelize, DataTypes) {
   var Teacher = sequelize.define('Teacher', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type : DataTypes.STRING,
+      validate : {
+        isUnique : (value, next) => {
+          Teacher.find({
+            where : {email : value},
+            attributes : ['id']
+          })
+          .done((err,user) => {
+            if(err)
+            return next('Email address already in use!');
+            next()
+          })
+        },
+          isEmail : {msg : 'Email format is incorrect!'}
+      }
+    },
     subject_id: DataTypes.INTEGER
   }, {
     classMethods: {
