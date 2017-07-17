64f6cda0d13e87afd8c6fc7b15eca0c3deaf055b
pdvega <pdvegaa@gmail.com>
2017-07-17 19:52:25 +0700

    seeding done

--- seeders/20170717111556-addUser.js
+++ seeders/20170717111556-addUser.js
@@ -12,6 +12,27 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
+    return queryInterface.bulkInsert('Users', [{
+      username : 'johndoe',
+      password : 'foobar',
+      role : 'teacher',
+      createdAt : new Date(),
+      updatedAt : new Date()
+    },
+    {
+      username : 'pakdengklek',
+      password : 'gogetgold',
+      role : 'academic',
+      createdAt : new Date(),
+      updatedAt : new Date()
+    },
+    {
+      username : 'charlesxavier',
+      password : 'magnetowhy',
+      role : 'headmaster',
+      createdAt : new Date(),
+      updatedAt : new Date()
+    }], {})
   },
 
   down: function (queryInterface, Sequelize) {
@@ -22,5 +43,7 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('Person', null, {});
     */
+   return queryInterface.bulkDelete('Users', null,
+   {});
   }
 };
